28cc168587dd72396fa9330fdd678913c5b15163
ubaidalishaikh <161380272+ubaidalishaikh@users.noreply.github.com>
2024-03-19 01:48:35 +0500

    Add files via upload
    I've completed 3rd Assighment 🎉 Governer Sindh IT Intiative Kamran Khan Tessori

---
+++ Q13.js
@@ -0,0 +1,6 @@
+"use strict";
+// Exersice No13
+let transportation = ['Civic Honda', 'Supra Mk4', 'Rollroyse', 'Buggati Chiron'];
+for (let index = 0; index < transportation.length; index++) {
+    console.log("I would like to own a " + transportation[index]);
+}
---
+++ Q13.ts
@@ -0,0 +1,7 @@
+// Exersice No13
+
+
+let transportation: string[] = ['Civic Honda','Supra Mk4','Rollroyse','Buggati Chiron'];
+for (let index = 0; index < transportation.length; index++) {
+    console.log("I would like to own a " + transportation[index]);
+}
